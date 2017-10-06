49ecbef9d8dce75289affd980c386205d20f4a35
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2017-10-06 16:42:24 +0200

    Ficheros base

---
+++ Práctica 2/constants.h
@@ -0,0 +1,35 @@
+#ifndef __TABLA_H
+#define __TABLA_H
+
+#define PRINCIPAL 256
+#define INICIOBLOQUE 257
+#define FINBLOQUE 258
+#define VAR 259
+#define FINVAR 260
+#define SI 261
+#define SINO 262
+#define MIENTRAS 263
+#define LEER 264
+#define ESCRIBIR 265
+#define DEVOLVER 266
+#define REPETIR 267
+#define HASTA 268
+#define PUNTOYCOMA 269
+#define COMA 270
+#define CORIZQ 271
+#define CORDER 272
+#define PARIZQ 273
+#define PARDER 274
+#define TIPOBASICO 275
+#define ASIGNACION 276
+#define SUMRES 277
+#define OPUNARIO 278
+#define OPBINARIO 279
+#define CONSTENTERA 280
+#define CONSTREAL 281
+#define CONSTCARACTER 282
+#define CONSTLOGICA 283
+#define CONSTCADENA 284
+#define IDENT 285
+
+#endif
