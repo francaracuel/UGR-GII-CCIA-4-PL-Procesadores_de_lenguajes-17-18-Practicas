cba22ac9f8c176440eb97189515160f5d9e5104d
Juan M. Fajardo <juanmfajardo@me.com>
2017-10-06 16:54:47 +0200

    Constants.h

--- Práctica 2/constants.h
+++ Práctica 2/constants.h
@@ -1,35 +1,34 @@
-#ifndef __TABLA_H
-#define __TABLA_H
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
 
 #define PRINCIPAL 256
-#define INICIOBLOQUE 257
-#define FINBLOQUE 258
-#define VAR 259
-#define FINVAR 260
-#define SI 261
-#define SINO 262
-#define MIENTRAS 263
-#define LEER 264
-#define ESCRIBIR 265
-#define DEVOLVER 266
-#define REPETIR 267
-#define HASTA 268
-#define PUNTOYCOMA 269
-#define COMA 270
-#define CORIZQ 271
-#define CORDER 272
-#define PARIZQ 273
-#define PARDER 274
-#define TIPOBASICO 275
-#define ASIGNACION 276
-#define SUMRES 277
-#define OPUNARIO 278
-#define OPBINARIO 279
-#define CONSTENTERA 280
-#define CONSTREAL 281
-#define CONSTCARACTER 282
-#define CONSTLOGICA 283
-#define CONSTCADENA 284
-#define IDENT 285
+#define INI_BLOQUE 257
+#define FIN_BLOQUE 258
+#define PUNTO_Y_COMA 259
+#define COMA 260
+#define PARENT_IZQUIERDO 261
+#define PARENT_DERECHO 262
+#define INI_VAR_LOCAL 263
+#define FIN_VAR_LOCAL 264
+#define ASIGNACION 265
+#define INI_DIM_MATRIZ 266
+#define FIN_DIM_MATRIZ 267
+#define SI 268
+#define SI_NO 269
+#define HACER 270
+#define HASTA 271
+#define MIENTRAS 272
+#define DEVOLVER 273
+#define TIPO_BASICO 274
+#define ENTRADA 275
+#define SALIDA 276
+#define SIGNO 277
+#define OP_UNARIO 278
+#define OP_BINARIO 279
+#define CONST_LOGICA 280
+#define CADENA 281
+#define CONST_ENTERO 282
+#define CONST_FLOTANTE 283
+#define CONST_CARACTER 284
 
 #endif
